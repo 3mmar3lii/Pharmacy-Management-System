2cc3183db712c6da988143c01d2c0d235e28488e
ali-1272004 <alie57217@gmail.com>
2026-03-07 17:11:53 +0200

    accept incomming changes from main

--- controllers/medicine.js
+++ controllers/medicine.js
@@ -3,14 +3,60 @@ const Medicine = require("../models/Medicine");
 const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 
+// let addMedicine = catchAsync(async (req, res, next) => {
+
+// //   if (!req.body.name || !req.body.price) {
+// //     return next(new AppError("Medicine name and price are required", 400));
+// //   }
+
+//   let imageUrl = "";
+
+//   if (req.file) {
+//     const response = await imagekit.upload({
+//       file: req.file.buffer,
+//       fileName: req.file.originalname,
+//       folder: "medicines"
+//     });
+
+//     imageUrl = response.url;
+//   }
+
+//   const medicine = await Medicine.create({
+//     ...req.body,
+//     image: imageUrl
+//   });
+
+//   res.status(201).json({
+//     status: "success",
+//     data: medicine
+//   });
+
+// });
+
 let addMedicine = catchAsync(async (req, res, next) => {
 
-  if (!req.body.name || !req.body.price) {
-    return next(new AppError("Medicine name and price are required", 400));
+  const {
+    nameEn,
+    nameAr,
+    price,
+    quantity,
+    description,
+    category,
+    brand,
+    expiryDate,
+    supplier,
+    requiresPrescription,
+    discount
+  } = req.body;
+
+  // validation
+  if (!nameEn || !nameAr || !price || !quantity || !description || !category || !brand) {
+    return next(new AppError("Missing required medicine fields", 400));
   }
 
-  let imageUrl = "";
+  let imageUrl = "default-medicine.png";
 
+  // upload image to imagekit
   if (req.file) {
     const response = await imagekit.upload({
       file: req.file.buffer,
@@ -22,7 +68,17 @@ let addMedicine = catchAsync(async (req, res, next) => {
   }
 
   const medicine = await Medicine.create({
-    ...req.body,
+    nameEn,
+    nameAr,
+    price,
+    quantity,
+    description,
+    category,
+    brand,
+    expiryDate,
+    supplier,
+    requiresPrescription,
+    discount,
     image: imageUrl
   });
 
